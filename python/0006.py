c2882908e54279c1d3473b3c2e7ad7d217b07812
gpna1229 <annaduen@gmail.com>
2026-03-14 18:01:55 +0800

    Create 0006.py

---
+++ python/0006.py
@@ -0,0 +1,13 @@
+"""
+    @param n1:{Integer}
+    @param n2:{Integer}
+    @return :{Boolean}
+"""
+def findGCD(n1, n2):
+    if n2 == 0:
+        return n1
+    else: 
+        return findGCD(n2, n1 % n2)
+        
+def findLCM(n1, n2):
+    return n1 * n2 /findGCD(n1, n2)
